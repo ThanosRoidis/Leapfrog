abcb3d267533128bc3e61f8b29c1721f790855f1
Stratos Makrigiannakis <stratos.makrigiannakis@gmail.com>
2017-08-06 20:25:24 +0300

    Added basic Platform scripts and prefabs

---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Movable))]
+
+public class MovingPlatform : BasePlatform {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
